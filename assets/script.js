b0967409746bca272280b20d455285102afb25df
bartleyf <32643835+bartleyf@users.noreply.github.com>
2017-12-06 15:19:42 -0600

    cleanup and troubleshooting

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,74 @@
-var cartoons = ["The Simpsons", "Family Guy", "Tom and Jerry", "Teenage Mutant Ninja Turtles", "GI Joe", "Fraggle Rock"];
+$(document).ready(function () {
+    var cartoons = ["The Simpsons", "Family Guy", "Tom and Jerry", "Teenage Mutant Ninja Turtles", "GI Joe", "Fraggle Rock"];
 
-function renderButtons() {
-
-    $(".buttons").empty();
-
-    for (var i = 0; i < cartoons.length; i++) {
-        var a = $("<button>");
-        a.addClass("cartoon");
-        a.attr("data-name", cartoons[i]);
-        a.text(cartoons[i]);
-        $(".buttons").append(a);
+    function alertCartoonName() {
+        console.log($(this).data("name"));
     }
-}
 
-$("#add-cartoon").on("click", function (event) {
-    event.preventDefault();
-    var cartoon = $("#cartoon-input").val().trim();
-    cartoons.push(cartoon);
+    function renderButtons() {
+
+        $(".buttons").empty();
+
+        for (var i = 0; i < cartoons.length; i++) {
+            var a = $("<button>");
+            a.addClass("cartoon");
+            a.attr("data-name", cartoons[i]);
+            a.text(cartoons[i]);
+            $(".buttons").append(a);
+        }
+    }
+
+    $("#add-cartoon").on("click", function (event) {
+        event.preventDefault();
+        var cartoon = $("#cartoon-input").val().trim();
+        cartoons.push(cartoon);
+        renderButtons();
+        console.log(cartoons);
+    });
+
     renderButtons();
-});
 
-renderButtons();
+    $(document).on("click", ".cartoon", alertCartoonName);
 
-$("button").on("click", function () {
-    var cartoon = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        cartoon + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $("button").on("click", function () {
+        var cartoon = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            cartoon + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    $(".images").empty();
+        $(".images").empty();
 
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .done(function (response) {
-            var results = response.data;
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function (response) {
+                var results = response.data;
+                console.log(queryURL);
 
-            for (var i = 0; i < results.length; i++) {
+                for (var i = 0; i < results.length; i++) {
 
-                var cartoonImage = $("<img>");
-                cartoonImage.attr("src", results[i].images.fixed_height_still.url);
-                cartoonImage.attr("data-still", results[i].images.fixed_height_still.url);
-                cartoonImage.attr("data-animate", results[i].images.fixed_height.url);
-                cartoonImage.attr("data-state", "still");
-                cartoonImage.addClass("gif");
+                    var cartoonImage = $("<img>");
+                    cartoonImage.attr("src", results[i].images.fixed_height_still.url);
+                    cartoonImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    cartoonImage.attr("data-animate", results[i].images.fixed_height.url);
+                    cartoonImage.attr("data-state", "still");
+                    cartoonImage.addClass("gif");
 
-                $(".images").append(cartoonImage);
-            }
-
-            $(".gif").on("click", function () {
-
-                var state = $(this).attr("data-state");
-
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
+                    $(".images").append(cartoonImage);
                 }
+
+                $(".gif").on("click", function () {
+
+                    var state = $(this).attr("data-state");
+
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
             });
-        });
+    });
 });
